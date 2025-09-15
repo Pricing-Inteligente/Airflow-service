f2e398685577087a994a4a30384c08eef5b498b0
santiago <santibote@gmail.com>
2025-09-15 17:42:20 -0500

    Host Docker Internal

--- dags/dag_scrapy.py
+++ dags/dag_scrapy.py
@@ -15,7 +15,7 @@ default_args = {
 }
 
 # Conexión a Mongo
-MONGO_URI = "mongodb://10.34.1.50:8580"  
+MONGO_URI = "mongodb://host.docker.internal:8580"  
 DB_NAME = "raw_productos"                 
 COLLECTION_NAME = "arroz" 
 
