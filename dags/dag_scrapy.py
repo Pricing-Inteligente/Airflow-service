36fca463893168a4b3ee41da097449acb37338e3
santiago <santibote@gmail.com>
2025-09-25 03:31:26 -0500

    Prueba variables test

--- dags/dag_scrapy.py
+++ dags/dag_scrapy.py
@@ -116,7 +116,7 @@ with DAG(
             docker_url="unix://var/run/docker.sock",
             network_mode="airflow_net",
             environment={
-                "IS_PROD": "False",
+                "IS_PROD": "True",
                 "MONGO_URI": MONGO_URI,
                 "MONGO_RESTART": "False",
             },
@@ -132,7 +132,7 @@ with DAG(
             docker_url="unix://var/run/docker.sock",
             network_mode="airflow_net",
             environment={
-                "IS_PROD": "False",
+                "IS_PROD": "True",
                 "MONGO_URI": MONGO_URI,
                 "MONGO_RESTART": "False",
             },
