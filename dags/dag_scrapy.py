70cb024e7997638d6b821bb3df6c42ead4d858d3
santiago <santibote@gmail.com>
2025-10-11 14:13:35 -0500

    False prod

--- dags/dag_scrapy.py
+++ dags/dag_scrapy.py
@@ -155,7 +155,7 @@ def mongo_to_pg(**kwargs):
     
 
 total_shards = 3
-image_name = "giancass07/scrapy-app:v1.2"
+image_name = "giancass07/scrapy-app:latest"
     
 with DAG(
     "scrapy_shards_dag",
